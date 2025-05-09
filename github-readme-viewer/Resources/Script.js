224412bbbae89bb4184e6a287b1ba91e7e3cc39d
kgruiz <kgruiz08@gmail.com>
2025-05-09 17:32:58 -0400

    Changed name

--- github-readme-viewer/Resources/Script.js
+++ github-readme-viewer/Resources/Script.js
@@ -1,13 +1,18 @@
 function show(enabled, useSettingsInsteadOfPreferences) {
+    const extensionName = 'GitHub README Exporter';
+
     if (useSettingsInsteadOfPreferences) {
-        document.getElementsByClassName('state-on')[0].innerText =
-            'GitHub README Viewer’s extension is currently on. You can turn it off in the Extensions section of Safari Settings.';
-        document.getElementsByClassName('state-off')[0].innerText =
-            'GitHub README Viewer’s extension is currently off. You can turn it on in the Extensions section of Safari Settings.';
-        document.getElementsByClassName('state-unknown')[0].innerText =
-            'You can turn on GitHub README Viewer’s extension in the Extensions section of Safari Settings.';
+        document.getElementsByClassName(
+            'state-on'
+        )[0].innerText = `${extensionName}’s extension is currently on. You can turn it off in the Extensions section of Safari Settings.`;
+        document.getElementsByClassName(
+            'state-off'
+        )[0].innerText = `${extensionName}’s extension is currently off. You can turn it on in the Extensions section of Safari Settings.`;
+        document.getElementsByClassName(
+            'state-unknown'
+        )[0].innerText = `You can turn on ${extensionName}’s extension in the Extensions section of Safari Settings.`;
         document.getElementsByClassName('open-preferences')[0].innerText =
-            'Quit and Open Safari Settings…'; // Ensure no " test"
+            'Quit and Open Safari Settings…';
     }
 
     if (typeof enabled === 'boolean') {
